c2af913e0efb1953432b2d957d064f97f9ed9869
varadtote <varadtote1111@gmail.com>
2023-10-19 01:10:44 +0530

    initial commit

---
+++ server_demo.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer((req, res) => {
+    if (req.url === "/") {
+        fs.readFile("./public/home.html", "UTF-8", function (err, html) {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(html);
+        });
+    }
+    else if (req.url === "/about") {
+        fs.readFile("./public/about.html", "UTF-8", function (err, html) {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(html);
+        });
+    }
+    else {
+        fs.readFile("./public/404.html", "UTF-8", function (err, html) {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(html);
+        });
+    }
+
+
+}).listen(5000, () => console.log('Server running....'))
